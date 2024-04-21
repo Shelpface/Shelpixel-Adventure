2f7efcbf5f691340df06188e412fd26f1236a11b
Shelpface <shelpface@gmail.com>
2024-04-22 00:04:22 +0300

    Update the Scene class

--- Engine/Scene.cs
+++ Engine/Scene.cs
@@ -1,22 +1,54 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace ShelpixelAdventure.Engine
 {
     public class Scene
     {
+        private List<GameObject> _children;
+
         public Scene()
         {
             
         }
 
+        public void AddChild(GameObject child)
+        {
+            child.Initialize(this);
+        }
+
+        public void AddInitializedChild(GameObject initChild)
+        {
+            if (initChild.IsInitialized)
+            {
+                _children.Add(initChild);
+            }
+            else
+            {
+                throw new System.Exception("You are trying to add an uninitialized GameObject! Use AddChild instead.");
+            }
+        }
+
         protected virtual void Update()
         {
-
+            if (_children != null)
+            {
+                foreach (var child in _children)
+                {
+                    child.Update();
+                }
+            }
         }
 
         protected virtual void Draw()
         {
-
+            if (_children != null )
+            {
+                foreach (var child in _children)
+                {
+                    child.Draw();
+                }
+            }
         }
 
     }
