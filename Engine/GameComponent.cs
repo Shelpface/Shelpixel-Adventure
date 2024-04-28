f28d829b351ff8c3346821922a1b651ffd9acd3f
Shelpface <shelpface@gmail.com>
2024-04-28 23:53:37 +0300

    Created GameComponent class

---
+++ Engine/GameComponent.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace ShelpixelAdventure.Engine
+{
+    public class GameComponent
+    {
+        private GameObject _parentGameObject;
+
+        private bool _isInitialized = false;
+
+        public GameObject ParentGO
+        {
+            get
+            {
+                return _parentGameObject;
+            }
+            private set
+            {
+                _parentGameObject = value;
+            }
+
+        }
+
+        public bool IsInitialized
+        {
+            get
+            {
+                return _isInitialized;
+            }
+            private set
+            {
+                _isInitialized = value;
+            }
+        }
+
+        public void Initialize(GameObject parentGameObject)
+        {
+            if (_isInitialized) throw new System.Exception(this + " has already been Initialized!");
+            IsInitialized = true;
+
+            _parentGameObject = parentGameObject;
+            //_parentGameObject.AddComponent(this)
+
+            Ready();
+        }
+
+        public virtual void Ready()
+        {
+
+        }
+
+        public virtual void Update() { }
+        public virtual void Draw(SpriteBatch spriteBatch) { }
+    }
+}
