7c7bc43e75db4ad072e26a290c9f3cdf5abec585
Shelpface <shelpface@gmail.com>
2024-04-29 17:54:57 +0300

    InputManager class has been done

---
+++ Engine/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace ShelpixelAdventure.Engine
+{
+    public static class InputManager
+    {
+        private static KeyboardState _previousKS;
+
+        public static bool IsKeyPressed(Keys key)
+        {
+            bool isKeyDown = Keyboard.GetState().IsKeyDown(key);
+            bool wasKeyDownInPreviousFrame = _previousKS.IsKeyDown(key);
+            _previousKS = Keyboard.GetState();
+
+            return isKeyDown && !wasKeyDownInPreviousFrame;
+        }
+
+        public static bool IsKeyDown(Keys key)
+        {
+            return Keyboard.GetState().IsKeyDown(key);
+        }
+    }
+}
