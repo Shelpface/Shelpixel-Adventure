68cb4c79960c7f841dc15420411d9e3a2072bc44
Shelpface <shelpface@gmail.com>
2024-05-05 12:53:33 +0300

    Fixed Load method in the SceneManager class

--- Engine/SceneManager.cs
+++ Engine/SceneManager.cs
@@ -6,17 +6,10 @@
 
         public static void Load(Scene scene)
         {
-            if (scene == null) throw new System.Exception("You are trying to load a scene with a null value. You need to instantiate the scene");
-            if (CurrentScene != scene)
-            {
-                CurrentScene = scene;
-            }
-            else
-            {
-                throw new System.Exception("Use Reload method if you want to reload current scene");
-            }
-        }
+            if (scene == null)
+                throw new System.Exception("You are trying to load a scene with a null value. You need to instantiate the scene");
 
-        // Need to implement the Reload method here
+            CurrentScene = scene;
+        }
     }
 }
